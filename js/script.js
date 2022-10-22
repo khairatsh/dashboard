0cc62c2545ffbe7a9df1c08c6019abd0e5591762
khairatsh <85008480+khairatsh@users.noreply.github.com>
2022-10-22 16:05:28 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+jQuery(function($) {
+    $(window).on('scroll', function() {
+		if ($(this).scrollTop() >= 200) {
+			$('.navbar').addClass('fixed-top');
+		} else if ($(this).scrollTop() == 0) {
+			$('.navbar').removeClass('fixed-top');
+		}
+	});
+	
+	function adjustNav() {
+		var winWidth = $(window).width(),
+			dropdown = $('.dropdown'),
+			dropdownMenu = $('.dropdown-menu');
+		
+		if (winWidth >= 768) {
+			dropdown.on('mouseenter', function() {
+				$(this).addClass('show')
+					.children(dropdownMenu).addClass('show');
+			});
+			
+			dropdown.on('mouseleave', function() {
+				$(this).removeClass('show')
+					.children(dropdownMenu).removeClass('show');
+			});
+		} else {
+			dropdown.off('mouseenter mouseleave');
+		}
+	}
+	
+	$(window).on('resize', adjustNav);
+	
+	adjustNav();
+});
+
+
+
+
+
+$(document).ready(function(){
+  $(".btn-primary").click(function(){
+    $(".collapse").collapse('toggle');
+  });
+  $(".btn-success").click(function(){
+    $(".collapse").collapse('show');
+  });
+  $(".btn-warning").click(function(){
+    $(".collapse").collapse('hide');
+  });
+});
